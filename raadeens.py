6ff30718317ddf53a7790c3b70f1a15473ff9c90
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-19 09:49:26 +0200

    bugs fixed

--- raadeens.py
+++ raadeens.py
@@ -2,6 +2,7 @@ import random
 import sys
 for j in range(20):
     num1 = random.randint(1,1000)
+    print(num1)
     score = 0
     ronde = 0
     ronde += 1
@@ -10,18 +11,25 @@ for j in range(20):
         if int(guess1) == num1:
             score + 1
             print("wow u heeft het nummer geraden!")
-        elif abs(num1 - int(guess1)) <= 50:
-            print("U zit warm!")
+            print("Ronde " + str(ronde) + " is afgelopen score= " + str(score))
+            spelen = input("Nog een keer spelen? 1 ja 2 nee")
+            if int(spelen) > 1:
+                print("Oke tot de volgende keer!" + sys.exit())
+            else:
+                print("Suc6 in de volgende ronde!")
+
         elif abs(num1 - int(guess1)) <= 20:
             print("U zit erg warm!")
+        elif abs(num1 - int(guess1)) <= 50:
+            print("U zit warm!")
         else:
             print("Ooh nee probeer het nog een keer")
     
-    print("Ronde " + str(ronde) + " is afgelopen score= " + str(score))
-    spelen = input("Nog een keer spelen? 1 ja 2 nee")
-    if int(spelen) > 1:
-        print("Oke tot de volgende keer!" + sys.exit())
-    else:
-        print("Suc6 in de volgende ronde!")
+        print("Ronde " + str(ronde) + " is afgelopen score= " + str(score))
+        spelen = input("Nog een keer spelen? 1 ja 2 nee")
+        if int(spelen) > 1:
+            print("Oke tot de volgende keer!" + sys.exit())
+        else:
+            print("Suc6 in de volgende ronde!")
 
 
