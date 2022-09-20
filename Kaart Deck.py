14848047385e28031ac55f1a03d0cf519bd8c512
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-20 09:33:00 +0200

    Kaart deck

---
+++ Kaart Deck.py
@@ -0,0 +1,11 @@
+mydeck = ["2","3","4","5","6","7","8","9","10","Aas","Boer","Vrouw","Heer","Joker"]
+mycards = ["Harten","Schoppen","Klaveren","Ruiten"]
+mymadedeck = []
+for j in mycards:
+    for i in mydeck:
+        mymadedeck.insert(1,j+i)
+        if len (mymadedeck)>54:
+            mymadedeck = mymadedeck[:54]
+            print(mymadedeck[0:7])
+            input("Enter om uw volledige deck te zien")
+            print(mymadedeck)
--- raadeens.py
+++ raadeens.py
@@ -25,11 +25,11 @@ for j in range(20):
         else:
             print("Ooh nee probeer het nog een keer")
     
-        print("Ronde " + str(ronde) + " is afgelopen score= " + str(score))
-        spelen = input("Nog een keer spelen? 1 ja 2 nee")
-        if int(spelen) > 1:
-            print("Oke tot de volgende keer!" + sys.exit())
-        else:
-            print("Suc6 in de volgende ronde!")
+    print("Ronde " + str(ronde) + " is afgelopen score= " + str(score))
+    spelen = input("Nog een keer spelen? 1 ja 2 nee")
+    if int(spelen) > 1:
+        print("Oke tot de volgende keer!" + sys.exit())
+    else:
+        print("Suc6 in de volgende ronde!")
 
 
