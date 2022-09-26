290f5f4ee9d95d5142561dbe759ab937a65809bd
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-26 10:01:34 +0200

    opdracht08

---
+++ Apparte map in mijn repo/opdracht08.py
@@ -0,0 +1,6 @@
+import fruitmand
+
+watermeloen ={"name":"watermeloen","weight":"2000","color":"green","round":True}
+fruitmand.fruitmand.append(watermeloen)
+for i in range(8):
+    print(fruitmand.fruitmand[i]['weight'])
