c387b5dc12b839ede1f84ab76a18cb14bb08c7ef
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-26 09:28:44 +0200

    fruitmand

--- Kaart Deck.py
+++ Kaart Deck.py
@@ -1,11 +1,19 @@
-mydeck = ["2","3","4","5","6","7","8","9","10","Aas","Boer","Vrouw","Heer","Joker"]
+import random
+
+
+mydeck = ["2","3","4","5","6","7","8","9","10","Aas","Boer","Vrouw","Heer",]
 mycards = ["Harten","Schoppen","Klaveren","Ruiten"]
 mymadedeck = []
+mymadedeck.append("Joker")
+mymadedeck.append("Joker")
 for j in mycards:
     for i in mydeck:
         mymadedeck.insert(1,j+i)
-        if len (mymadedeck)>54:
-            mymadedeck = mymadedeck[:54]
-            print(mymadedeck[0:7])
-            input("Enter om uw volledige deck te zien")
-            print(mymadedeck[7:54])
+        print(len(mymadedeck))
+    mymadedeck = mymadedeck[:54]
+    random.shuffle(mymadedeck)
+    for i in range(7):
+        i+=1
+        print("Kaart " +str(i) + ": " + str( mymadedeck[int(i)]))
+    print("deck (47 kaarten)" + str(mymadedeck[7:54]))
+    print(len(mymadedeck))
