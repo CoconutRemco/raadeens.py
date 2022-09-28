1bacdc39081e719f751ba31456218b80eb6b4a0a
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-09-28 10:29:58 +0200

    opdracht12

--- Apparte map in mijn repo/opdracht11.py
+++ Apparte map in mijn repo/opdracht11.py
@@ -1,10 +1,23 @@
 import fruitmand
-weight = 0
+rond = 0
+nietrond = 0
 kleur = input('Welke kleur wilt u? yellow, green, orange, red, brown? ').lower()
+if kleur == 'yellow' or kleur == 'green' or kleur == 'orange' or kleur == 'red' or kleur == 'brown':
+    pass
+else:
+    print('HEY ' +kleur+ ' IS GEEN KLEUR UIT DE LIJSTTT')
 
 kleuren = sorted(fruitmand.fruitmand, key=lambda x: x['color'], reverse = True)
 for i in kleuren:
     if i['color'] == kleur:
-        weight += (i)['weight']
-print(weight)
+      if i['round'] == True:
+        rond += 1
+      else:
+        nietrond += 1
+if rond > nietrond:
+    print('Wow er zijn'+ str(rond-nietrond) + 'meer ronde dan niet ronde fruitsoorten van deze kleur')
+elif rond == nietrond:
+    print("Wow er zijn evenveel ronde " +str(rond)+ " als niet ronde "+str(nietrond)+ " van deze kleur!")
+elif rond < nietrond:
+    print("Wow er zijn "+ str(nietrond-rond) + "meer niet ronde dan ronde van deze kleur!")
     
--- Apparte map in mijn repo/opdracht12.py
+++ Apparte map in mijn repo/opdracht12.py
@@ -1,2 +1,20 @@
 import fruitmand
-
+langstefruit = {"name":""}
+for f in fruitmand.fruitmand:
+    if len(langstefruit["name"]) < len(f['name']):
+        langstefruit = f
+naamlangstefruit = langstefruit['name']
+gewichtlangstefruit = langstefruit['weight']/1000
+kleurlangstefruit = langstefruit['color']
+lengtenaamlangstefruit= len(langstefruit['name'])
+if kleurlangstefruit == 'orange':
+    kleurlangstefruit = 'oranje'
+elif kleurlangstefruit == 'brown':
+    kleurlangstefruit = 'bruin'
+elif kleurlangstefruit == 'green':
+    kleurlangstefruit = 'groen'
+elif kleurlangstefruit == 'red':
+    kleurlangstefruit = 'red'
+else:
+    kleurlangstefruit = 'yellow'
+print("De " + naamlangstefruit + " ("+ str(lengtenaamlangstefruit) + " Letters) Heeft een " + kleurlangstefruit + " kleur en een gewicht van " + str(gewichtlangstefruit) + "KG")
