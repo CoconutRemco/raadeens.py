e6f5d2a937e522cbb078c2f4dcf7dfd421de7155
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-10-13 09:44:50 +0200

    namenenleeftijden

--- namenenleeftijden.py
+++ namenenleeftijden.py
@@ -1,8 +1,10 @@
 listnamenenleeftijden = []
+leeftijden = []
 def namenenleeftijden_func():
     namenenleeftijden = {}
     naam = input("Wat is uw naam?")
-    leeftijd = input("Wat is uw leeftijd?")     
+    leeftijd = input("Wat is uw leeftijd?")
+    leeftijden.append(leeftijd)     
     namenenleeftijden[naam] = leeftijd
     return namenenleeftijden
 var1 = True
@@ -14,7 +16,12 @@ while var1 == True:
     else:
         listnamenenleeftijden.append(dict1)
 for i in range(len(listnamenenleeftijden)):
-    print(listnamenenleeftijden[i])
+    for keys in listnamenenleeftijden[i]:
+        print(keys + " is " + leeftijden[i] + " jaar")
+
+        
+    
+        
 
        
         
