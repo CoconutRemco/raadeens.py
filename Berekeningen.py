2678b37a742d5171c8ad4db09d1fef4caf519dc3
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-10-18 15:53:18 +0200

    commit

---
+++ Berekeningen.py
@@ -0,0 +1,12 @@
+from audioop import add
+
+
+n1 = int(input("Welk nummer wilt u"))
+n2 = int(input("Welk nummer wilt u"))
+
+def addition():
+    ad = n1+n2
+    return ad
+
+var1=addition()
+print(var1)
