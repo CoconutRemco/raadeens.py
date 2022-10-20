6767846ead661f8076e532110208f9d1508e36f0
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-10-20 13:22:06 +0200

    updates

--- Apparte map in mijn repo/opdracht07.py
+++ Apparte map in mijn repo/opdracht07.py
@@ -1,5 +1,5 @@
 import fruitmand
 
-for i in range(7):
+for i in range(len(fruitmand.fruitmand)):
     if fruitmand.fruitmand[int(i)]['round'] == True:
         print(fruitmand.fruitmand[int(i)]['name'])
--- test2.py
+++
@@ -1,7 +0,0 @@
-def getWelcome(welcomeTo:str='town') -> str:
-    return 'Welcome to function '+welcomeTo
-
-
-where = 'space'
-message = getWelcome(where)
-print(message)
