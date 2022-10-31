8e4c066064518a5dce4e0a047b10efc032872008
CoconutRemco <112873034+CoconutRemco@users.noreply.github.com>
2022-10-31 14:13:45 +0100

    Fixes

--- Fibonacci reeks2.py
+++ Fibonacci reeks2.py
@@ -1,5 +1,18 @@
-def recur_fibo(n):
+import random
+
+
+def berekenfibonacci(n):
    if n <= 1:
        return n
    else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
+       return(berekenfibonacci(n-1) + berekenfibonacci(n-2))
+
+nterms = random.randint(4,30)
+
+
+if nterms <= 0:
+   print("Plese enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   for i in range(nterms):
+       print(berekenfibonacci(i))
